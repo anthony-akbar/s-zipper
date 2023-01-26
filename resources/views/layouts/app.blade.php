eb38b0d283d9700fad965febccb5e08a54b610bb
Anthony Akbar <mamadjanovakbar@gmail.com>
2023-01-26 12:15:02 +0500

    FIM Integrated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,56 +1,57 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('/main/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('/main/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('/main/css/mdb.css') }}">
-    <link rel="stylesheet" href="{{ asset('/main/css/mdb.lite.css') }}">
-    <link rel="stylesheet" href="{{ asset('/main/css/style.css') }}">
+    <title>S. Zipper Accessories</title>
+    <link rel="stylesheet" href="{{ asset('assets/front-face/vendor/bootstrap/css/bootstrap.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/front-face/vendor/owlcarousel/dist/assets/owl.carousel.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/front-face/vendor/fancybox/dist/jquery.fancybox.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/admin/vendor/toastr/toastr.min.css') }} "/>
+    <link rel="stylesheet" href="{{ asset('assets/front-face/css/style.min.css') }} "/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/front-face/fonts/flaticon/font/flaticon.css') }} ">
+    <link href="{{ asset('assets/front-face/vendor/jqvmap/dist/jqvmap.css') }}" media="screen" rel="stylesheet" type="text/css">
+
 </head>
-<body style="background-color: black">
+<body>
+
+
 @include('partials.navbar')
 @yield('content')
-<script type="text/javascript" src="{{ asset('/main/js/bootstrap.js')}}"></script>
-<script type="text/javascript" src="{{ asset('/main/js/jquery.js')}}"></script>
-<script type="text/javascript" src="{{ asset('/main/js/mdb.js')}}"></script>
-<script type="text/javascript" src="{{ asset('/main/js/mdb.lite.js')}}"></script>
-<script type="text/javascript" src="{{ asset('/main/js/propper.js')}}"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-<script>
-    function onScroll(){
-        if($('body').scrollTop()<=50) {
-            $('.navbar').removeClass('navbar-scrolled');
-            $('.navbar').removeClass('navbar-light');
-            $('.navbar').addClass('navbar-dark');
-        } else {
-            $('.navbar').addClass('navbar-scrolled');
-            $('.navbar').removeClass('navbar-dark');
-            $('.navbar').addClass('navbar-light');
+
+<script type="text/javascript" src="{{ asset('assets/front-face/vendor/jqvmap/dist/jquery.vmap.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/front-face/vendor/jqvmap/dist/maps/jquery.vmap.world.js') }}" charset="utf-8"></script>
+
+<script src="{{ asset('assets/front-face/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/vendor/owlcarousel/dist/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/vendor/fancybox/dist/jquery.fancybox.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/js/main-script.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/vendor/toastr/toastr.min.js') }}"></script>
+<script src="{{ asset('assets/front-face/js/magic-ajax.js') }}"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        if ($("#vmap").length > 0) {
+            jQuery("#vmap").vectorMap({
+                map: "world_en",
+                backgroundColor: "white",
+                color: "#f3f3f3",
+                zoomOnScroll: true,
+                markersSelectable: false,
+                markersSelectableOne: false,
+                enableZoom: true,
+                onRegionClick: function (e) {
+                    e.preventDefault();
+                },
+                hoverColor: "#051548",
+                hoverOpacity: null,
+                normalizeFunction: "linear",
+                selectedColor: "#051548",
+                selectedRegions: ["TR", "KZ", "DK", "RO", "AE", "QA", "MK", "RS", "CS", "SN", "SE", "GH", "CZ", "JO", "LB", "BG", "SI", "AT", "FR", "UK", "GB", "DE", "NL", "RU", "IL", "CY", "GR", "GE", "AZ", "IQ", "EG", "BA", "XS", "LY", "DZ", "TN", "MA", "BY", "NG", "UA", "BG", "RO", "US", "UZ", "AL", "ML", "SE", "AT", "PL", "IT"],
+                showTooltip: true
+            });
         }
-    }
-    function reveal() {
-
-        var reveals = document.querySelectorAll(".from-left, .from-right, .down-to-up, .up-to-down");
-
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    window.addEventListener("scroll", reveal);
-
+    });
 </script>
 </body>
 </html>
---
+++ resources/views/partials/slider.blade.php
@@ -0,0 +1,23 @@
+<div class="main-slider-container">
+    <div class="main-slider owl-carousel">
+        <a href="/">
+            <div class="item"
+                 style="background-image: url(/uploads/sliders/thumb/1920x1080/5e7f1f9f39f650f2f642a174f40f2807-780.jpg)"></div>
+        </a><a href="/">
+            <div class="item"
+                 style="background-image: url(/uploads/sliders/thumb/1920x1080/deda690c03de5d6cfe936aa13fcccea0-661.jpg)"></div>
+        </a><a href="/">
+            <div class="item"
+                 style="background-image: url(/uploads/sliders/thumb/1920x1080/f6cfc4861c34dc6cb313a0cfd5df6336-1234.jpg)"></div>
+        </a>
+        <a href="#">
+            <div class="item">
+                <video autoplay loop muted style="width: 100%;height: auto;">
+                    <source src="/assets/front-face/img/core-img/videos/tiny-fim-slider.mp4" type="video/mp4">
+                    <source src="/assets/front-face/img/core-img/videos/fim-slider.ogm" type="video/ogg">
+                </video>
+                <div class="overlay"></div>
+            </div>
+        </a></div>
+    <div class="bottom-message">SWIPE</div>
+</div>
