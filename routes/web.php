6fe29e7641ee2f7639f63bfdc96ce94d69a22f50
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2023-01-28 17:04:48 +0500

    category

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $sliders = \App\Models\SliderItem::all();
-    return view('dashboard.index', compact('sliders'));
+    $categories = \App\Models\ZipperCategory::all();
+    return view('dashboard.index', compact('sliders','categories'));
 });
 
-Route::group(['prefix' => 'admin', 'namespace'=>'admin'], function () {
-    Route::get('/', function () {return view('admin.dashboard');})->name('admin.home');
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('admin.home');
     Route::group(['prefix' => 'slider'], function () {
         Route::get('/', [SliderController::class, 'index'])->name('admin.slider');
         Route::post('/store', [SliderController::class, 'store'])->name('admin.slider.store');
         Route::delete('/{id}', [SliderController::class, 'destroy'])->name('admin.slider.delete');
     });
-    Route::group(['prefix'=>'categories'], function () {
-       Route::get('/', [CategoryController::class, 'index'])->name('admin.categories');
-       Route::post('/store', [CategoryController::class, 'store'])->name('admin.categories.store');
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.categories');
+        Route::post('/store', [CategoryController::class, 'store'])->name('admin.categories.store');
         Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('admin.categories.delete');
         Route::get('/{id}', [CategoryController::class, 'show'])->name('admin.categories.show');
     });
